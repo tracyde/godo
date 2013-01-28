d11292c043a2f91fdac2c59402a5f02d4a449de5
Derek Tracy <tracyde@gmail.com>
2013-01-27 23:14:32 -0500

    fleshing out functionality of godo and trying to get the structs to properly encode as a gob file for storage.

--- collection/collection.go
+++ collection/collection.go
@@ -1,22 +1,107 @@
+// Package collection provides the types to manage godo projects and tasks.
 package collection
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
 
 type Task struct {
 	description string
 	priority    int
 }
 
+type Tasks []*Task // Wrapper type to allow sorting
+
 type Project struct {
 	name        string
 	description string
 	priority    int
-	tasks       []Task
+	tasks       Tasks
 }
+
+type Projects []*Project // Wrapper type to allow sorting
 
 type Collection struct {
-	filename string
-	projects []Project
+	Filename string
+	projects Projects
 }
 
-func New(filename string) *Collection {
-	return &Collection{filename}
+// Wrapper types to allow different sort orders for Projects type.
+type ByName struct{ Projects }
+type ByPriority struct{ Projects }
+
+// Methods required by sort.Interface for ByName type, allows
+// Projects to be sorted by name.
+func (s ByName) Less(i, j int) bool {
+	return s.Projects[i].name < s.Projects[j].name
+}
+
+// Methods required by sort.Interface for ByPriority type, allows
+// Projects to be sorted by priority.
+func (s ByPriority) Less(i, j int) bool {
+	return s.Projects[i].priority < s.Projects[j].priority
+}
+
+// Methods required by sort.Interface for Projects type, need
+// to cast them as either ByName or ByPriority.
+func (p Projects) Len() int {
+	return len(p)
+}
+func (p Projects) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+// Methods required by sort.Interface for Tasks type, default
+// sort is by priority.
+func (t Tasks) Len() int {
+	return len(t)
+}
+func (t Tasks) Less(i, j int) bool {
+	return t[i].priority < t[j].priority
+}
+func (t Tasks) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+// Method returns a pointer to an empty Collection.
+func New(f string) *Collection {
+	return &Collection{Filename: f, projects: make([]*Project, 0)}
+}
+
+// Method to allow easy sorting of an entire Collection.
+func (c *Collection) Sort() {
+	sort.Sort(ByPriority{c.projects})
+	for _, p := range c.projects {
+		sort.Sort(p.tasks)
+	}
+}
+
+func (c *Collection) AddProject(n, d string, p int) {
+	c.projects = append(c.projects, &Project{n, d, p, make([]*Task, 0)})
+}
+
+func (c *Collection) AddTask(pn, d string, p int) error {
+	sort.Sort(ByName{c.projects})
+	i := sort.Search(len(c.projects), func(i int) bool { return c.projects[i].name >= pn })
+	//fmt.Printf("AddTask-> sort.Search found index: %d\n", i)
+	if i < len(c.projects) && c.projects[i].name == pn {
+		//fmt.Printf("AddTask-> Adding new Task to project: %s\n", c.projects[i].name)
+		c.projects[i].tasks = append(c.projects[i].tasks, &Task{d, p})
+	} else {
+		return errors.New("project name does not exist")
+	}
+	return nil
+}
+
+func (c *Collection) Print() {
+	c.Sort()
+	for i, p := range c.projects {
+		fmt.Printf("%d. %s - %s [%d]\n", i, p.name, p.description, p.priority)
+		for i, t := range p.tasks {
+			fmt.Printf("\t%d. %s [%d]\n", i, t.description, t.priority)
+		}
+		fmt.Println()
+	}
 }
--- godo.go
+++ godo.go
@@ -2,12 +2,60 @@ package main
 
 import (
 	"fmt"
-
+	//	"os"
+	"bytes"
+	"encoding/gob"
 	"github.com/tracyde/godo/collection"
+	"io/ioutil"
 )
 
-func main() {
-	c := collection.New("/tmp/test")
+func TestLoad(c *collection.Collection) {
+	c.AddProject("Test0", "This is a test Project0", 2)
+	c.AddProject("Test1", "This is a test Project1", 3)
+	c.AddProject("Test2", "This is a test Project2", 4)
+	c.AddProject("Test3", "This is a test Project3", 1)
 
-	fmt.Printf("Type: %T  ::  Value: %v\n", c, c)
+	c.AddTask("Test0", "This task should be on Test0 project", 2)
+	c.AddTask("Test0", "Task for Test0 project", 3)
+	c.AddTask("Test2", "Task for Test2 project", 3)
+	c.AddTask("Test3", "Test3 task ready for action", 1)
+	c.AddTask("Test1", "Test1 task ready for action", 1)
+	c.AddTask("Test3", "Test3 I wonder why this project is so popular", 2)
+	/*
+		err := c.AddTask("Test4", "complete adding func for tasks", 6)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "logging:", err)
+		}
+	*/
+}
+
+func main() {
+	c := collection.New("/tmp/test.gob")
+	TestLoad(c)
+	c.Print()
+
+	fmt.Println("Saving collection to gobfile")
+	b := new(bytes.Buffer)
+	enc := gob.NewEncoder(b)
+	enc.Encode(c)
+	err := ioutil.WriteFile(c.Filename, b.Bytes(), 0600)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("just saved gob with %v\n", c)
+
+	n, err := ioutil.ReadFile(c.Filename)
+	if err != nil {
+		panic(err)
+	}
+	p := bytes.NewBuffer(n)
+	dec := gob.NewDecoder(p)
+	e := collection.New("/tmp/test.gob")
+	err = dec.Decode(&e)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("just read gob from file and it's showing: %v\n", e)
+	e.Print()
+	// fmt.Printf("Type: %T  ::  Value: %v\n", c, c)
 }
