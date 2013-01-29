08ed2d714dba7827dd459f3ca89d3b27f4f5b4fe
Derek Tracy <tracyde@gmail.com>
2013-01-28 23:32:49 -0500

    made all struct fields exported so they get serialized properly by gob

--- collection/collection.go
+++ collection/collection.go
@@ -8,24 +8,24 @@ import (
 )
 
 type Task struct {
-	description string
-	priority    int
+	Description string
+	Priority    int
 }
 
 type Tasks []*Task // Wrapper type to allow sorting
 
 type Project struct {
-	name        string
-	description string
-	priority    int
-	tasks       Tasks
+	Name        string
+	Description string
+	Priority    int
+	Tasks       Tasks
 }
 
 type Projects []*Project // Wrapper type to allow sorting
 
 type Collection struct {
 	Filename string
-	projects Projects
+	Projects Projects
 }
 
 // Wrapper types to allow different sort orders for Projects type.
@@ -33,15 +33,15 @@ type ByName struct{ Projects }
 type ByPriority struct{ Projects }
 
 // Methods required by sort.Interface for ByName type, allows
-// Projects to be sorted by name.
+// Projects to be sorted by Name.
 func (s ByName) Less(i, j int) bool {
-	return s.Projects[i].name < s.Projects[j].name
+	return s.Projects[i].Name < s.Projects[j].Name
 }
 
 // Methods required by sort.Interface for ByPriority type, allows
-// Projects to be sorted by priority.
+// Projects to be sorted by Priority.
 func (s ByPriority) Less(i, j int) bool {
-	return s.Projects[i].priority < s.Projects[j].priority
+	return s.Projects[i].Priority < s.Projects[j].Priority
 }
 
 // Methods required by sort.Interface for Projects type, need
@@ -54,12 +54,12 @@ func (p Projects) Swap(i, j int) {
 }
 
 // Methods required by sort.Interface for Tasks type, default
-// sort is by priority.
+// sort is by Priority.
 func (t Tasks) Len() int {
 	return len(t)
 }
 func (t Tasks) Less(i, j int) bool {
-	return t[i].priority < t[j].priority
+	return t[i].Priority < t[j].Priority
 }
 func (t Tasks) Swap(i, j int) {
 	t[i], t[j] = t[j], t[i]
@@ -67,28 +67,28 @@ func (t Tasks) Swap(i, j int) {
 
 // Method returns a pointer to an empty Collection.
 func New(f string) *Collection {
-	return &Collection{Filename: f, projects: make([]*Project, 0)}
+	return &Collection{Filename: f, Projects: make([]*Project, 0)}
 }
 
 // Method to allow easy sorting of an entire Collection.
 func (c *Collection) Sort() {
-	sort.Sort(ByPriority{c.projects})
-	for _, p := range c.projects {
-		sort.Sort(p.tasks)
+	sort.Sort(ByPriority{c.Projects})
+	for _, p := range c.Projects {
+		sort.Sort(p.Tasks)
 	}
 }
 
 func (c *Collection) AddProject(n, d string, p int) {
-	c.projects = append(c.projects, &Project{n, d, p, make([]*Task, 0)})
+	c.Projects = append(c.Projects, &Project{n, d, p, make([]*Task, 0)})
 }
 
 func (c *Collection) AddTask(pn, d string, p int) error {
-	sort.Sort(ByName{c.projects})
-	i := sort.Search(len(c.projects), func(i int) bool { return c.projects[i].name >= pn })
+	sort.Sort(ByName{c.Projects})
+	i := sort.Search(len(c.Projects), func(i int) bool { return c.Projects[i].Name >= pn })
 	//fmt.Printf("AddTask-> sort.Search found index: %d\n", i)
-	if i < len(c.projects) && c.projects[i].name == pn {
-		//fmt.Printf("AddTask-> Adding new Task to project: %s\n", c.projects[i].name)
-		c.projects[i].tasks = append(c.projects[i].tasks, &Task{d, p})
+	if i < len(c.Projects) && c.Projects[i].Name == pn {
+		//fmt.Printf("AddTask-> Adding new Task to project: %s\n", c.Projects[i].Name)
+		c.Projects[i].Tasks = append(c.Projects[i].Tasks, &Task{d, p})
 	} else {
 		return errors.New("project name does not exist")
 	}
@@ -97,10 +97,10 @@ func (c *Collection) AddTask(pn, d string, p int) error {
 
 func (c *Collection) Print() {
 	c.Sort()
-	for i, p := range c.projects {
-		fmt.Printf("%d. %s - %s [%d]\n", i, p.name, p.description, p.priority)
-		for i, t := range p.tasks {
-			fmt.Printf("\t%d. %s [%d]\n", i, t.description, t.priority)
+	for i, p := range c.Projects {
+		fmt.Printf("%d. %s - %s [%d]\n", i, p.Name, p.Description, p.Priority)
+		for i, t := range p.Tasks {
+			fmt.Printf("\t%d. %s [%d]\n", i, t.Description, t.Priority)
 		}
 		fmt.Println()
 	}
